b62c61a463b864ee6c0029a287defed67d777792
Larrylai <larryhua@msn.com>
2022-02-28 02:39:24 +0800

    Updated Huoshen Engine api
    Huoshan Machine translation seem to perform better than the Baidu API

--- Extract&Translate with huoshan.py
+++ Extract&Translate with huoshan.py
@@ -4,39 +4,41 @@ import requests
 import random
 import json
 from hashlib import md5
+from volcengine.ApiInfo import ApiInfo
+from volcengine.Credentials import Credentials
+from volcengine.ServiceInfo import ServiceInfo
+from volcengine.base.Service import Service
+import time
 
-# 设置翻译API的账号和密码 BAIDU Setup your APIid and Appkey acquired from baidu API
-appid = ''
-appkey = ''
+# 设置翻译API的账号和密码 Huoshan https://www.volcengine.com/docs
+k_access_key = "paste your access key here"
+k_secret_key = "paste your secret key here"
+k_timeout = 5  # second
 
-# 设置从A语音翻译到B语言，其他语言码查看  If you need more language code refer to:`https://api.fanyi.baidu.com/doc/21`
-from_lang = 'en'
-to_lang =  'zh'
+k_service_info = \
+    ServiceInfo('open.volcengineapi.com',
+                {'Content-Type': 'application/json'},
+                Credentials(k_access_key, k_secret_key, 'translate', 'cn-north-1'),
+                5,
+                5)
+k_query = {
+    'Action': 'TranslateText',
+    'Version': '2020-06-01'
+}
 
-#组合翻译的地址 sent your translation to translate to here
-endpoint = 'http://api.fanyi.baidu.com'
-path = '/api/trans/vip/translate'
-url = endpoint + path
-
-#example query
-query = 'Hello World! This is 1st paragraph.\nThis is 2nd paragraph.'
-
-# Generate salt and sign 组装校验码
-def make_md5(s, encoding='utf-8'):
-    return md5(s.encode(encoding)).hexdigest()
-
-
+k_api_info = {
+    'translate': ApiInfo('POST', '/', k_query, {}, {})
+}
+service = Service(k_service_info, k_api_info)
 
 
 #前面都是调用翻译API的部分 Everything about machine translation is above
 
-
-
 #下面是提取XML的部分 Everything about extracting texts from Xmls is underneath
 
 
-#从xml文本里面提取需要翻译的key extract key from XML
-with open("totsk_troops.xml", "r", encoding='utf-8') as f:
+#从xml文本里面提取需要翻译的key
+with open("sandboxcore_spnpccharacters.xml", "r", encoding='utf-8') as f:
     origin_content = f.read()
 strings_src = re.findall(r'"(.*?)"', origin_content)
 strings = list()
@@ -44,19 +46,19 @@ for string in strings_src:
     if '=' in string:
         strings.append(string)
 
-#从打开模板XML open Template XML
-with open("template.xml", "r", encoding='utf-8') as f:
+#从打开模板XML
+with open("templateCNs.xml", "r", encoding='utf-8') as f:
     template = BeautifulSoup(f.read(), 'xml')
 common = template.find('strings')
 
-#获取提取前的string id Extract String ID before extraction
+#获取提取前的string id
 def get_id(_string):
     index_right = _string.index('}')
     index_left = _string.index('=')
     temp = _string[index_left + 1:index_right].replace(" ","_")
     return temp.replace("'","_")
 
-#用text生成id并且替换 Generate new id based on old text
+#用text生成id并且替换
 def replace_id(_string):
     index_left = _string.index('}')
     temp= _string[index_left + 1:].replace(" ","_")
@@ -66,27 +68,24 @@ def replace_id(_string):
     return temp
 
 
-
-
-
-
 def get_string(_string):
     index_left = _string.index('}')
     return _string[index_left + 1:]
 
+#translate single line
 def translate(_string):
     print(_string)
     query = _string
-    salt = random.randint(32768, 65536)
-    sign = make_md5(appid + query + str(salt) + appkey)
-    # Build request
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    payload = {'appid': appid, 'q': query, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
-
+    body = {
+        'SourceLanguage': 'en',
+        'TargetLanguage': 'zh',
+        'TextList': [query],
+    }
+    r = service.json('translate', {}, json.dumps(body))
     # Send request
-    r = requests.post(url, params=payload, headers=headers)
-    result = r.json()
-    translate_result = result["trans_result"][0]["dst"]
+    result = json.loads(r)
+    print(result)
+    translate_result = result['TranslationList'][0]['Translation']
     print(translate_result)
     return translate_result
 
@@ -94,16 +93,16 @@ def translate(_string):
     #print(json.dumps(result, indent=4, ensure_ascii=False))
 
 
+
 for string in strings:
     # enable this line to make new id from name
     #tag = template.new_tag(name='string', attrs={'id': replace_id(string).lower(), 'text': get_string(string)})
     original = get_string(string)
     translation = translate(original)
-
     # enable this line to read original String IDs
     tag = template.new_tag(name='string', attrs={'id': get_id(string), 'text': translation})
     common.append(tag)
+    time.sleep(0.05)
 
-
-with open("totsk_troops_CNs.xml", "w+", encoding='utf-8') as f:
+with open("sandboxcore_spnpccharacters_CNs.xml", "w+", encoding='utf-8') as f:
     f.write(template.prettify())
